3c099a784d27620d47b5ded5be1f91c5916d3f7a
ajay krishna <krishna@opsys.cs.umsl.edu>
2024-02-06 23:15:58 -0600

    child

--- user.c
+++ user.c
@@ -1,10 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+
 int main(int argc, char *argv[]) {
-	int itr = atoi(argv[1]);
-	for (int i = 1; i<= itr; i++) {
-		printf("User PID:%d PPID: %d Iterations:%d before sleeping\n", getpid(), getpid(), i);
-		sleep(1);
-		printf("User PID: %d PPID: %d Iterations:%d after sleeping\n", getpid(), getpid(), getpid(), i);
-	}
-return 0;
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
+
+    // Convert the argument to integer
+    int iterations = atoi(argv[1]);
+    //Printing iterations and PID of parent and child
+    for (int i = 1; i <= iterations; i++) {
+        printf("USER PID:%d PPID:%d Iteration:%d before sleeping\n", pid, ppid, i);
+        sleep(1);
+        printf("USER PID:%d PPID:%d Iteration:%d after sleeping\n", pid, ppid, i);
+    }
+
+    return 0;
 }
+
